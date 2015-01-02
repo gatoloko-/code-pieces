a4260a01ec1b73f2fd221f6aa2c979b73c485337
gatoloko- <ricomo@outlook.com>
2015-01-02 12:21:26 -0400

    multiple assoc array from mysql rows

---
+++ associative array with sigle or multiple mysql rows.php
@@ -0,0 +1,15 @@
+<?php
+	public function getArray($search){
+	include 'plug.php';	
+	$data = array();
+	$query = "SELECT origin FROM tag WHERE slug LIKE '%".$search."%'";
+	$resultado = $plug->query($query);
+	while($row = $resultado->fetch_array(MYSQLI_ASSOC)){
+		$data[] = $row;
+	}		
+	$plug->close();
+		
+	return $data;
+}
+
+?>
